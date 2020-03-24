6376a901b746411c6811b2e985c7b8bea0036933
lambdafate <lambdafate@163.com>
2020-03-24 21:42:54 +0800

    🔍 not real binary search

---
+++ 0081-搜索螺旋排序数组2/Solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        if(nums == null){ return false;}
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i] == target){ return true;}
+        }
+        return false;
+    }   
+}
