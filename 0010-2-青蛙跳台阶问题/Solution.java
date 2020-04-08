aeccaa437d9fc56487e04b7daa3b99e518567e2f
lambdafate <lambdafate@163.com>
2020-04-08 10:23:06 +0800

    :smile: recursive

---
+++ 0010-2-青蛙跳台阶问题/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numWays(int n) {
+        if(n < 2){
+            return 1;
+        }
+        int[] dp = new int[n+1];
+        dp[0] = 0; dp[1] = 1; dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i-1] +dp[i-2];
+            dp[i] %= 1000000007;
+        }
+        return dp[n];
+    }
+}
