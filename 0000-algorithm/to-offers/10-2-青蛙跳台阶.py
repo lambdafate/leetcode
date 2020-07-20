f749c0691c5c8ec6600e517aed21e3c156e516e4
lambdafate <lambdafate@163.com>
2020-07-20 18:11:04 +0800

    update

--- 0000-algorithm/to-offers/10-2-青蛙跳台阶.py
+++ 0000-algorithm/to-offers/10-2-青蛙跳台阶.py
@@ -1,4 +1,16 @@
 class Solution:
+    # 每次求解只需要前面的两个解
+    def numWays(self, n: int) -> int:
+        if n < 2:
+            return 1
+        i, j = 1, 1
+        for _ in range(2, n+1):
+            j, i = i+j, j
+            j %= 1000000007
+        return j
+
+
+    """
     def numWays(self, n: int) -> int:
         if n < 2:
             return 1
@@ -9,3 +21,4 @@ class Solution:
             dp[i] = dp[i-1] + dp[i-2]
             dp[i] %= 1000000007
         return dp[n]
+    """
